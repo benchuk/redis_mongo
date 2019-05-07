a1b4446ee75352281435e91410fde4819fefe33f
benchuk <benchuk@gmail.com>
2019-05-07 10:48:49 +0300

    .

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ const MongoClient = require('mongodb').MongoClient
 var redis = require('redis'),
   client = redis.createClient()
 var geoRedis = require('georedis').initialize(client)
+const fs = require('fs')
 
 const url = 'mongodb://localhost:27017'
 const dbName = 'spatialTest'
 const shouldInsert = false
 //const shouldInsert = false
 const runMongo = true
-const runRedis = false
-const amount = 100000
-const DISTANCE = 1000000
+const runRedis = true
+const amount = 10000
+const DISTANCE = 100000
 const queryLat = 31.1
 const queryLon = 31.1
 const latDelta = 0.0001
@@ -78,11 +79,11 @@ function mongoDbInsertSpatialItems(db, lat, lon, count) {
 
     lat += latDelta
     lon += lonDelta
-    //console.log('lat: ' + lat)
-    //console.log('lon: ' + lon)
+    console.log('lat: ' + lat)
+    console.log('lon: ' + lon)
 
     var newItem = {
-      name: 'item ' + i,
+      name: 'item_' + i,
       location: {
         type: 'Point',
         coordinates: [lat, lon]
@@ -116,34 +117,56 @@ function mongoDbInsertSpatialItems(db, lat, lon, count) {
 function mongoDbRunSpatialTest(db, cb) {
   let distance = DISTANCE / 1000.0 / 6378.137
   console.log('distance: ' + distance)
+  //   db.collection('places')
+  //     // .find({
+  //     //   location: { $nearSphere: [queryLat, queryLon], $minDistance: distance }
+  //     // })
+  //     // .find({
+  //     //   location: {
+  //     //     $nearSphere: {
+  //     //       $geometry: { type: 'Point', coordinates: [queryLat, queryLon] },
+  //     //       $minDistance: 0,
+  //     //       $maxDistance: DISTANCE
+  //     //     }
+  //     //   }
+  //     // })
+  //     .find({
+  //       location: {
+  //         $near: {
+  //           $geometry: { type: 'Point', coordinates: [queryLat, queryLon] },
+  //           $minDistance: 0,
+  //           $maxDistance: DISTANCE
+  //         }
+  //       }
+  //     })
+
   db.collection('places')
-    // .find({
-    //   location: { $nearSphere: [queryLat, queryLon], $minDistance: distance }
-    // })
-    // .find({
-    //   location: {
-    //     $nearSphere: {
-    //       $geometry: { type: 'Point', coordinates: [queryLat, queryLon] },
-    //       $minDistance: 0,
-    //       $maxDistance: DISTANCE
-    //     }
-    //   }
-    // })
-    .find({
-      location: {
-        $near: {
-          $geometry: { type: 'Point', coordinates: [queryLat, queryLon] },
-          $minDistance: 0,
-          $maxDistance: DISTANCE
+    .aggregate([
+      {
+        $geoNear: {
+          spherical: true,
+          near: { type: 'Point', coordinates: [queryLat, queryLon] },
+          distanceField: 'calculated',
+          maxDistance: DISTANCE,
+          includeLocs: 'location',
+          limit: 10000000
         }
       }
-    })
+    ])
     .toArray(function(err, docs) {
       console.log(err)
       console.log(
         '******************************* mongo nearby locations: ' + docs.length
       )
       console.log(docs[docs.length - 1])
+      let data = []
+      for (let item of docs) {
+        data.push({
+          latitude: item.location.coordinates[0],
+          longitude: item.location.coordinates[1]
+        })
+      }
+      saveToFile('mongo.csv', data)
       cb()
     })
 }
@@ -175,12 +198,11 @@ function RunRedisTests(geoRedis, count) {
 function redisInsertSpatialItems(geo, lat, lon, count) {
   console.log('inserting redis')
   for (var i = 0; i < count; i++) {
-    //console.log('i: ' + i)
-
+    console.log('i: ' + i)
     lat += latDelta
     lon += lonDelta
-    //console.log('lat: ' + lat)
-    //console.log('lon: ' + lon)
+    console.log('lat: ' + lat)
+    console.log('lon: ' + lon)
 
     geo.addLocation('item_' + i, { latitude: lat, longitude: lon }, function(
       err,
@@ -197,7 +219,7 @@ function geoRedisRunSpatialTest(geo, cb) {
   var options = {
     withCoordinates: true, // Will provide coordinates with locations, default false
     //withHashes: true, // Will provide a 52bit Geohash Integer, default false
-    //withDistances: true, // Will provide distance from query, default false
+    withDistances: true, // Will provide distance from query, default false
     //order: 'ASC', // or 'DESC' or true (same as 'ASC'), default false
     units: 'm', // or 'km', 'mi', 'ft', default 'm'
     //count: 100, // Number of results to return, default undefined
@@ -215,7 +237,20 @@ function geoRedisRunSpatialTest(geo, cb) {
           locations.length
         )
       console.log(locations[locations.length - 1])
+      console.log(locations[0])
+      saveToFile('redis.csv', locations)
       cb()
     }
   )
+}
+
+function saveToFile(fileName, data) {
+  var stream = fs.createWriteStream(fileName)
+  stream.once('open', function(fd) {
+    for (let item of data) {
+      stream.write(`${item.latitude},${item.longitude}\n`)
+    }
+
+    stream.end()
+  })
 }
